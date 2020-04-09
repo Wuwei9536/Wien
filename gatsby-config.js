e1623d2833368e9c465fd783aabf400c00a2762c
wien <wien.wuwei@gmail.com>
2020-04-09 09:01:38 +0800

    feat: gatsby-plugin-typescript/offline/manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Wien`,
+    description: `.`,
+    author: `@Wien`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,17 +18,26 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Wien`,
+        short_name: `Wien`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        cache_busting_mode: "none",
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-offline",
+      options: {
+        workboxConfig: {
+          globPatterns: ["**/*"],
+        },
+      },
+    },
+    `gatsby-plugin-typescript`,
   ],
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,39 +12,39 @@ const IndexPage = () => (
     <SEO title="Home" />
     <Image style={{ height: "100px", width: "100px", borderRadius: "50%" }} />
     <h1>A Front-end Development Engineer</h1>
-    <div class="container">
-      <div class="card">
-        <div class="imgBx">
+    <div className="container">
+      <div className="card">
+        <div className="imgBx">
           <img
             src="https://image.flaticon.com/icons/svg/2092/2092063.svg"
             alt=""
           />
         </div>
-        <div class="contentBx">
+        <div className="contentBx">
           <h2>Blog</h2>
           <p>Just Test.</p>
         </div>
       </div>
-      <div class="card">
-        <div class="imgBx">
+      <div className="card">
+        <div className="imgBx">
           <img
             src="https://image.flaticon.com/icons/svg/1197/1197460.svg"
             alt=""
           />
         </div>
-        <div class="contentBx">
+        <div className="contentBx">
           <h2>Code</h2>
           <p>Just Test.</p>
         </div>
       </div>
-      <div class="card">
-        <div class="imgBx">
+      <div className="card">
+        <div className="imgBx">
           <img
             src="https://image.flaticon.com/icons/svg/1067/1067256.svg"
             alt=""
           />
         </div>
-        <div class="contentBx">
+        <div className="contentBx">
           <h2>Launch</h2>
           <p>Just Test.</p>
         </div>
