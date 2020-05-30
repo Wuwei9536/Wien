c79ba668f8c3826d9bb5eda45538de45c95c5ee5
wien <wien.wuwei@gmail.com>
2020-05-31 00:08:51 +0800

    update logo & add link to FastRTC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -23,11 +22,16 @@ const IndexPage = () => (
           </div>
           <div className={styles.contentBx}>
             <h2>Blog</h2>
-            <p>Just Test.</p>
+            <p>暂时没有太多时间</p>
           </div>
         </div>
       </Link>
-      <div className={styles.card}>
+      <div
+        className={styles.card}
+        onClick={() => {
+          window.open("https://fastrtc.wuwei.tech/")
+        }}
+      >
         <div className={styles.imgBx}>
           <img
             src="https://image.flaticon.com/icons/svg/1197/1197460.svg"
@@ -35,8 +39,8 @@ const IndexPage = () => (
           />
         </div>
         <div className={styles.contentBx}>
-          <h2>Code</h2>
-          <p>Just Test.</p>
+          <h2>FastRTC</h2>
+          <p>基于WebRTC的视频通讯平台</p>
         </div>
       </div>
       <div className={styles.card}>
